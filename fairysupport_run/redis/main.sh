dafdad816519d953724df7736cab7c676094e40e
fairysupport <aoper@LAPTOP-V7L5ACHU>
2020-07-28 22:27:49 +0900

    commit

--- fairysupport_run/redis/main.sh
+++ fairysupport_run/redis/main.sh
@@ -5,6 +5,11 @@
 # check
 yum_installed_exit "redis.x86_64" "redis is already installed"
 
+suffix=""
+if [ $# -ge 1 ]; then
+  suffix=".${1}"
+fi
+
 sudo -S yum -y install epel-release
 sudo -S rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
 
@@ -12,7 +17,7 @@ sudo -S rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
 sudo -S yum -y install --enablerepo=remi redis
 
 # replace redis.conf
-bk_cp_mode_label "./redis.conf" "/etc/redis.conf" 644 etc_t
+bk_cp_mode_label "./redis.conf${suffix}" "/etc/redis.conf" 644 etc_t
 
 # set firewalld
 firewalld_add_rich 'rule family="ipv4" source address="192.168.1.0/24" port port="6379" protocol="tcp" accept'
